86b530aeac57160f5481723408d9d2b4134be975
Roni Choudhury <roni.choudhury@kitware.com>
2021-12-12 23:24:12 -0500

    Implement fuzzy search matching

--- nightshade/__init__.py
+++ nightshade/__init__.py
@@ -1,16 +1,37 @@
+from bs4 import BeautifulSoup
 import requests
 import sys
 import urllib
 
 
-def get_movie_url(search):
+def get_movies(search):
     url = f"https://www.rottentomatoes.com/search?search={urllib.parse.quote(search)}"
 
     r = requests.get(url)
     if r.status_code != 200:
         raise RuntimeError("Bad request")
 
-    return r.text
+    doc = BeautifulSoup(r.text, 'html.parser')
+    slot = doc.find("search-page-result", attrs={"slot": "movie"})
+    results = slot.find("ul").find_all("search-page-media-row")
+
+    return [{"year": r.get("releaseyear"),
+             "name": r.find_all("a")[1].string.strip(),
+             "href": r.find_all("a")[1].get("href")}
+            for r in results]
+
+
+def match_movie(movies, name, year=None):
+    def matches(m):
+        target = m["name"].lower()
+        search = name.lower()
+
+        name_matches = search == target or search in target
+        year_matches = year is None or year == m["year"]
+
+        return name_matches and year_matches
+
+    return next(filter(matches, movies), None)
 
 
 def test_cli():
@@ -18,5 +39,11 @@ def test_cli():
     if len(sys.argv) > 1:
         search = sys.argv[1]
 
-    print(get_movie_url(search))
+    year = None
+    if len(sys.argv) > 2:
+        year = sys.argv[2]
+
+    movies = get_movies(search)
+    print(match_movie(movies, search, year))
+
     return 0
