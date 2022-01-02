aa4a3b5fe0bd1dfe879024899c771be4cecdd4fc
Roni Choudhury <roni.choudhury@kitware.com>
2022-01-01 23:41:02 -0500

    Enable URL search mode

--- nightshade/csv.py
+++ nightshade/csv.py
@@ -1,4 +1,4 @@
-import csv
+import csv as csvv
 import click
 import json
 import sys
@@ -25,12 +25,12 @@ def get_year(text: str) -> Optional[int]:
 def csv(input_file, failure_file, success_file, skip):
     # Open the failure file for appending.
     with open(failure_file, "a") as fail:
-        writer = csv.writer(fail)
+        writer = csvv.writer(fail)
 
         with open(success_file, "a") as success:
             # Open the CSV file.
             with open(input_file, newline="") as input_stream:
-                reader = csv.reader(input_stream)
+                reader = csvv.reader(input_stream)
 
                 for i, row in enumerate(reader):
                     # Honor the skip parameter.
@@ -40,25 +40,37 @@ def csv(input_file, failure_file, success_file, skip):
                     # Extract the title, year, and any notes.
                     title = row[0]
                     year = get_year(row[1])
-                    notes = row[2]
+                    url = row[2]
+                    notes = row[3]
 
-                    # Perform a search for the title.
+                    matches = None
                     year_text = "" if year is None else f" ({year})"
-                    print(f"({i}) Searching for '{title}{year_text}'...", end="", file=sys.stderr, flush=True)
-                    movies = get_movies(title)
-                    matches = match_movie(movies, title, year)
-                    print("done", file=sys.stderr)
+                    if url:
+                        print(f"({i}) Retrieving data from {url} for '{title}{year_text}'...", end="", file=sys.stderr, flush=True)
+                        movie = get_movie_data(url)
+                        matches = [movie] if movie.title == title and (year is None or movie.year == year) else []
+                        print("done", file=sys.stderr)
+                    else:
+                        # Perform a search for the title.
+                        print(f"({i}) Searching for '{title}{year_text}'...", end="", file=sys.stderr, flush=True)
+                        movies = get_movies(title)
+                        matches = match_movie(movies, title, year)
+                        print("done", file=sys.stderr)
 
                     # If there is only one hit, record it and move on.
                     if len(matches) == 1:
                         data = get_movie_data(matches[0].href).dict()
                         data["notes"] = notes
+                        data["original"] = f"{title},{year or ''}"
                         print(json.dumps(data), file=success)
                     else:
                         writer.writerow(row)
 
                         if len(matches) == 0:
-                            print("    (no matches found)", file=fail)
+                            if url:
+                                print(f"    {movie.title},{movie.year},{url},{notes}", file=fail)
+                            else:
+                                print("    (no matches found)", file=fail)
                         else:
                             for m in matches:
-                                print(f"    {m.title},{m.year},{notes}", file=fail)
+                                print(f"    {m.title},{m.year},{url},{notes}", file=fail)
