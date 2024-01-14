76d736cbc12058646cfd67c2c07e60e43578b31a
Roni Choudhury <roni.choudhury@kitware.com>
2024-01-13 20:39:42 -0500

    Remove rotten tomatoes support code

--- nightshade/models.py
+++ nightshade/models.py
@@ -2,20 +2,6 @@ import pydantic
 from typing import List, Optional
 
 
-class MovieResult(pydantic.BaseModel):
-    year: Optional[int]
-    title: str
-    href: pydantic.HttpUrl
-
-
-class MovieData(MovieResult):
-    audience: Optional[int]
-    tomatometer: Optional[int]
-    rating: Optional[str]
-    genres: List[str]
-    runtime: Optional[int]
-
-
 class TMDBSearchResult(pydantic.BaseModel):
     id: int
     title: str
